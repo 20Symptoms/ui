3f89ae0ac4d32b38affea417d8880b106fe1e09a
Jace Vermillion <jacevermillion@Jaces-MacBook-Pro.local>
2020-02-16 15:58:33 -0500

    Initial commit, just getting rough template

---
+++ home/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <print>20 Symptoms</print>
+        <textarea>
+
+
+        </textarea>
+        <button>Yes</button>
+        <button>No</button>
+        <button>Maybe</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
